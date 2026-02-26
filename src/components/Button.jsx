94160b46685d338cfa9599f5622329053a61e073
Jose Emilio Enriquez Torres <105244719+JoseEmilioEnriquezTorres@users.noreply.github.com>
2026-02-26 13:51:41 -0600

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,40 @@
+const stylesButtonPrimary = {
+    backgroundColor: '#cc3ee4',
+    borderRadius: '10px',
+    color: '#ffffff',
+    border: 'none',
+    padding: '2px 10px'
+};
+
+const stylesButtonSecondary = {
+    backgroundColor: '#ededed',
+    borderRadius: '10px',
+    color: '#363636',
+    border: 'none',
+    padding: '2px 10px'
+};
+
+function Button({type, action, text = "Wihooo"}) {
+
+  return (
+    <button
+      /*className={props.type === "primary" ?
+        "button-primary" : "button-secondary"
+      }*/
+
+    style={type === "primary" ?
+     stylesButtonPrimary : {
+        ...stylesButtonSecondary,
+        backgroundColor: '#363636',
+        color: '#ffffff'
+     }
+    }
+
+    onClick={action ? action : () => alert("Wihooo")}
+    >
+      <p>{text}</p>
+    </button>
+  );
+}
+
+export default Button;
